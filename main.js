9f7cbb8f57eb0e5d796f3b1e08181a3f675d6945
javohir1606 <javohirallayorov1606@gmail.com>
2024-08-09 17:33:24 +0500

    main

---
+++ main.js
@@ -0,0 +1,48 @@
+// const x = (str, num) => {
+//    let result = "";
+//    for (let i = 0; i < num; i++){
+//     result += str[i];
+//    }
+//    return result;
+// }
+
+// console.log(x('lorem', 3));
+
+
+
+// const sliceStr = (str, str1) => {
+//    let result = "";
+//    for (let i = 1; i < str.length; i++){
+//     result += str[i];
+//    }
+//    return result;
+// }
+
+// const x = (str, str1) => sliceStr(str) + sliceStr(str1)
+
+// console.log(x('Hello', "There"));
+
+
+
+// oddly t/f l/y
+// function str(a) {
+//   let strL = false;
+//   let strY = false;
+//   for(i = 0; i < a.length; i++){
+//     if(a[i] == 'l'){
+//       strL = true;
+//     }
+//     if(a[i] == 'y'){
+//       strY = true;
+//     }
+//   }
+//   return strL && strY;
+// }
+
+// console.log(str("oddly"));
+
+
+// Uygi-vazifa
+// 1 - misol
+let n = 10;
+let m = 5;
